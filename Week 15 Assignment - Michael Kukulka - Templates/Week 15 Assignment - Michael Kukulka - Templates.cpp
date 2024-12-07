797a106f02bc8c1f644bb9f10b57e4ce30a4868b
Sorre <angrepenguin@gmail.com>
2024-12-06 22:08:24 -0500

    implemented templated and overloaded function to halve a provided double/float/int

--- Week 15 Assignment - Michael Kukulka - Templates/Week 15 Assignment - Michael Kukulka - Templates.cpp
+++ Week 15 Assignment - Michael Kukulka - Templates/Week 15 Assignment - Michael Kukulka - Templates.cpp
@@ -2,10 +2,29 @@
 // CIS 1202 N01
 // December 06, 2024
 
-#include <iomanip>
 #include <iostream>
-#include <vector>
+
+template<typename T>
+T half(T num);
+int half(int num);
 
 int main() {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	std::cout << half(a) << "\n";
+	std::cout << half(b) << "\n";
+	std::cout << half(c) << "\n";
+
 	return EXIT_SUCCESS;
+}
+
+template<typename T>
+T half(T num) {
+	return num / 2;
+}
+
+int half(int num) {
+	return round(static_cast<double>(num) / 2);
 }
